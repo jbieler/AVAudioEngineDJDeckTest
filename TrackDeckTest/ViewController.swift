b65a1b77fc72dceb7bf97af52d02761b142cbf52
Jochen Bieler <jochen.bieler@web.de>
2014-12-15 12:47:42 +0100

    Initial Commit

---
+++ TrackDeckTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrackDeckTest
+//
+//  Created by Jochen Bieler on 15.12.14.
+//  Copyright (c) 2014 Jochen Bieler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
